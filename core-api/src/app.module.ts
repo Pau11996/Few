81c035b15f7320abf7b8e0f314c7e5463895812c
Paul11996 <kazak.92@internet.ru>
2022-09-02 15:58:15 +0300

    Create project!

--- core-api/src/app.module.ts
+++ core-api/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database:process.env.MYSQL_NAME,
+      entities: [__dirname + '/**/*/.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
