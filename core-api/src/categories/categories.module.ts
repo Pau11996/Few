09065d620d99a65e5bfe6a071fc5113a9b0a7cfb
Paul11996 <kazak.92@internet.ru>
2022-10-04 16:52:15 +0300

    Fixed.

--- core-api/src/categories/categories.module.ts
+++ core-api/src/categories/categories.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { Category } from './entities/category.entity';
 
 @Module({
   providers: [CategoriesService],
   controllers: [CategoriesController],
+  imports: [TypeOrmModule.forFeature([Category]), forwardRef(() => AuthModule)],
+  exports: [CategoriesService],
 })
 export class CategoriesModule {}
